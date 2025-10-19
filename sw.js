3b8c1ae5d16cf7e01230a3afb48410336874e252
Rawle30 <rachene@gmail.com>
2025-10-19 02:19:12 -0400

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+// sw.js
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('journal-cache').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'style.css',
+        'script.js',
+        'manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
