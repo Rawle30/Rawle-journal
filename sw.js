f3f81a870e5369c32f3b0856b8f036658ae09442
Rawle30 <rachene@gmail.com>
2025-10-19 16:34:06 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,41 @@
+```javascript
+const CACHE_NAME = 'journal-cache-v2';
+const urlsToCache = [
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('journal-cache-v1').then(cache => { // Versioned cache
-      return cache.addAll([
-        'index.html',
-        'style.css',
-        'script.js',
-        'manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => {
+        return caches.match('index.html');
+      });
     })
   );
 });
+```;
 
 
